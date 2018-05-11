731b8e81e77856b7b90e4ef5e48c0bb69df6c228
Phoenix Wu <phoenix@funpodium.net>
2018-05-11 13:05:08 +0800

    Assignment 01

---
+++ Assignment_01/Assignment_01/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Assignment_01
+//
+//  Created by Phoenix Wu on H30/05/11.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import Foundation
---
+++ Assignment_01/Assignment_01/ConcentrationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment_01
+//
+//  Created by Phoenix Wu on H30/05/11.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import UIKit
+
+class ConcentrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
